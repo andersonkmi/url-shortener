d640ebeec100bc60e5ae070d719066976c36ee87
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-09-10 22:16:52 +0200

    Add docker db

---
+++ url-shortner-db/docker/init.sql
@@ -0,0 +1,7 @@
+CREATE SEQUENCE urlid_seq;
+
+CREATE TABLE url (
+    urlid bigint not null primary key default nextval('urlid_seq'),
+    url varchar not null unique,
+    short_url varchar not null
+);
