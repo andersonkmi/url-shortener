1afb25f7358f45f1dae1284c79ab73ca281761b5
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-09-19 23:01:56 +0200

    Add endpoints

--- url-shortner-api/src/main/java/org/codecraftlabs/shorturl/service/UrlShortnerService.java
+++ url-shortner-api/src/main/java/org/codecraftlabs/shorturl/service/UrlShortnerService.java
@@ -1,9 +1,14 @@
 package org.codecraftlabs.shorturl.service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +19,20 @@ import java.util.Map;
 @RestController
 @RequestMapping("/url-shortner/v1")
 public class UrlShortnerService {
+
+    @PostMapping(value = "/url", produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<URLResponse> createShortenedUrl(@RequestBody URLRequest urlRequest) {
+        URLResponse response = new URLResponse();
+        response.setUrl(urlRequest.getUrl());
+        response.setShortUrl("short here");
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping(value = "/url/{shortenedUrl}", produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getdUrl(@PathVariable String shortenedUrl) {
+        return ResponseEntity.ok("test");
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
