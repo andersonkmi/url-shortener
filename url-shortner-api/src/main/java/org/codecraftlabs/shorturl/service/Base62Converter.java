ad85442bc53dab52f79d4b48a5fe951446f8c90c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-09-20 08:14:03 +0200

    Make class package private

--- url-shortner-api/src/main/java/org/codecraftlabs/shorturl/service/Base62Converter.java
+++ url-shortner-api/src/main/java/org/codecraftlabs/shorturl/service/Base62Converter.java
@@ -2,17 +2,15 @@ package org.codecraftlabs.shorturl.service;
 
 import javax.annotation.Nonnull;
 
-public class Base62Converter {
+class Base62Converter {
     private static final String BASE62_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final int BASE = 62;
-
 
     @Nonnull
     public String toBase62(long number) {
         if (number == 0) {
             return "0";
         }
-
         StringBuilder result = new StringBuilder();
         while (number > 0) {
             int remainder = (int) (number % BASE);
