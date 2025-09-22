e365f0e4b14db5fd2e8ec63e9d981396b6df62e8
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-09-22 22:50:36 +0200

    Add initial logs

--- url-shortner-api/src/main/java/org/codecraftlabs/shorturl/api/UrlShortnerRestController.java
+++ url-shortner-api/src/main/java/org/codecraftlabs/shorturl/api/UrlShortnerRestController.java
@@ -2,6 +2,8 @@ package org.codecraftlabs.shorturl.api;
 
 import org.codecraftlabs.shorturl.service.URLShorteningException;
 import org.codecraftlabs.shorturl.service.URLShortnerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,6 +28,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/url-shortner/v1")
 public class UrlShortnerRestController {
+    private static final Logger logger = LoggerFactory.getLogger(UrlShortnerRestController.class);
     private final URLShortnerService urlShortnerService;
 
     @Autowired
@@ -36,14 +39,19 @@ public class UrlShortnerRestController {
     @PostMapping(value = "/url", produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<URLResponse> createShortenedUrl(@RequestBody URLRequest urlRequest) {
         try {
+            logger.info("Creating a short URL for {}", urlRequest.toString());
             String shortUrl = urlShortnerService.generateShortUrl(urlRequest.getUrl());
             URLResponse response = new URLResponse();
             response.setUrl(urlRequest.getUrl());
             response.setShortUrl(shortUrl);
+
+            logger.info("Short URL created: '{}'", shortUrl);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (URLShorteningException exception) {
+            logger.error("Fail to generate short URL", exception);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "URL not shortened", exception);
         } catch (Exception exception) {
+            logger.error("An internal service error has occurred", exception);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal system error", exception);
         }
     }
