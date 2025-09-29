93c1594ad6b74592efe0e4aab4c8d6510bd57ab5
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2025-09-29 20:27:33 +0200

    Add unit tests for rest controller

--- url-shortner-api/src/main/java/org/codecraftlabs/shorturl/api/UrlShortnerRestController.java
+++ url-shortner-api/src/main/java/org/codecraftlabs/shorturl/api/UrlShortnerRestController.java
@@ -8,22 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.Nonnull;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/url-shortner/v1")
@@ -63,17 +57,5 @@ public class UrlShortnerRestController {
         return originalUrl.
                 <ResponseEntity<String>>map(s -> ResponseEntity.status(HttpStatus.FOUND).location(URI.create(s)).build())
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 }
--- url-shortner-api/src/test/java/org/codecraftlabs/shorturl/api/UrlShortnerRestControllerTest.java
+++ url-shortner-api/src/test/java/org/codecraftlabs/shorturl/api/UrlShortnerRestControllerTest.java
@@ -1,7 +1,9 @@
 package org.codecraftlabs.shorturl.api;
 
 import org.assertj.core.api.Assertions;
+import org.codecraftlabs.shorturl.service.URLShorteningException;
 import org.codecraftlabs.shorturl.service.URLShortnerService;
+import org.codecraftlabs.shorturl.service.repository.DatabaseException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,9 +12,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 @ExtendWith(MockitoExtension.class)
 public class UrlShortnerRestControllerTest {
@@ -32,12 +37,57 @@ public class UrlShortnerRestControllerTest {
     }
 
     @Test
-    public void when_url_found_should_return_200() {
+    public void when_url_found_should_return_302() {
         // Setup mocks
         Mockito.when(urlShortnerService.getOriginalUrl("TM65")).thenReturn(Optional.of("http://www.test.com"));
 
         ResponseEntity<String> response = urlShortnerRestController.getUrl("TM65");
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
         Assertions.assertThat(response.getHeaders().getLocation()).isEqualTo(URI.create("http://www.test.com"));
+    }
+
+    @Test
+    public void when_short_url_generated_should_return_201() {
+        // Setup mock
+        Mockito.when(urlShortnerService.generateShortUrl("http://www.test.com")).thenReturn("TM65");
+
+        URLRequest request = new URLRequest();
+        request.setUrl("http://www.test.com");
+        var response = urlShortnerRestController.createShortenedUrl(request);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        var body = response.getBody();
+        Assertions.assertThat(body).isNotNull();
+        Assertions.assertThat(body.getUrl()).isEqualTo("http://www.test.com");
+        Assertions.assertThat(body.getShortUrl()).isEqualTo("TM65");
+    }
+
+    @Test
+    public void when_url_shortening_exception_should_return_400() {
+        // Setup mock
+        Mockito.when(urlShortnerService.generateShortUrl("http://www.test.com"))
+                .thenThrow(new URLShorteningException("Failed to process", new DatabaseException("Failed to access database")));
+
+        URLRequest request = new URLRequest();
+        request.setUrl("http://www.test.com");
+        var exception = catchThrowable(() -> urlShortnerRestController.createShortenedUrl(request));
+        Assertions.assertThat(exception).isInstanceOf(ResponseStatusException.class);
+
+        ResponseStatusException originalException = (ResponseStatusException) exception;
+        Assertions.assertThat(originalException.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void when_other_exception_should_return_500() {
+        // Setup mock
+        Mockito.when(urlShortnerService.generateShortUrl("http://www.test.com"))
+                .thenThrow(new NullPointerException("Test null pointer"));
+
+        URLRequest request = new URLRequest();
+        request.setUrl("http://www.test.com");
+        var exception = catchThrowable(() -> urlShortnerRestController.createShortenedUrl(request));
+        Assertions.assertThat(exception).isInstanceOf(ResponseStatusException.class);
+
+        ResponseStatusException originalException = (ResponseStatusException) exception;
+        Assertions.assertThat(originalException.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
